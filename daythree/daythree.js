4be828134a0a48ebc538e19e5a6dad8e31d83ea4
Sakshi Nandu kamod <89306707+SAKSHIKAMOD@users.noreply.github.com>
2023-04-20 22:19:23 +0530

    first commit

---
+++ daythree/daythree.js
@@ -0,0 +1,41 @@
+//single-line comment
+/*multi-line comment*/
+
+/*alert("Congratulations")
+alert("Hello");
+alert(4+2);    
+let num=15;
+  alert(num)
+const a=167;
+  alert(a);
+a=122;
+  alert(a);*/
+
+//assignment operator
+let a=2 
+    a*=5;
+alert(a)
+
+//relational operator
+var res=12>0;
+  alert(res)
+
+//logical operator
+var x=13;
+var y=12;
+alert(x==y && x>y)
+
+//if else
+if(x>y)
+     alert("Sakshi")
+else
+     alert("Kamod")
+
+//find type of data
+alert(typeof(1));
+
+//prompt --> used to take input box
+let age=prompt("Whats your age?",100)
+    alert("Your age is "+age)
+
+//insert image 
